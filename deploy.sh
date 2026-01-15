21dbaab8127ff78238e4a03f177f452bcd8a3f85
bratuhin <bratuhin-kostya99@mail.ru>
2026-01-15 23:10:06 +0300

    переделал по рекоммендации таймвеб

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
-#!/bin/bash
+DEBIAN_FRONTEND=noninteractive apt-get update && apt-get install -y --no-install-recommends \
+curl \
+build-essential \
+libpq-dev \
+&& rm -rf /var/lib/apt/lists/*
 
-pip3 install -r requirements.txt
-
-echo "END"
+pip install -r requirements.txt
