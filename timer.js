e4d4c5ebd16a7dcf8e1c801851e80f172c9335e5
bratuhin <bratuhin-kostya99@mail.ru>
2026-02-07 12:30:35 +0300

    просто нерабочий таймер, как шаблон для нормального правильного
    Signed-off-by: bratuhin <bratuhin-kostya99@mail.ru>

---
+++ timer.js
@@ -0,0 +1,13 @@
+(function(){
+	var _id="6b23f2b64430cf0e6d5173cc57664ff2";
+	while(document.getElementById("timer"+_id))_id=_id+"0";
+		document.write("<div id='timer"+_id+"' style='min-width:238px;
+			height:58px;
+'></div>");var _t=document.createElement("script");
+_t.src="//megatimer.ru/timer/timer.min.js?v=1";
+var _f=function(_k){var l=new MegaTimer(_id, {"view":[1,1,1,1],"type":{"currentType":"1","params":{"usertime":true,"tz":"3","utc":1783004400000}},"design":{"type":"circle","params":{"width":"1","radius":"27","line":"solid","line-color":"#ffffff","background":"opacity","direction":"direct","number-font-family":{"family":"Port Lligat Slab","link":"<link href='//fonts.googleapis.com/css?family=Port+Lligat+Slab' rel='stylesheet' type='text/css'>"},"number-font-size":"30","number-font-color":"#ffffff","separator-margin":"0","separator-on":false,"separator-text":":","text-on":true,"text-font-family":{"family":"Port Lligat Slab","link":"<link href='//fonts.googleapis.com/css?family=Port+Lligat+Slab' rel='stylesheet' type='text/css'>"},"text-font-size":"11","text-font-color":"#ffffff"}},"designId":5,"theme":"white","width":238,"height":58});if(_k!=null)l.run();};_t.onload=_f;
+_t.onreadystatechange=function(){if(_t.readyState=="loaded")_f(1);
+};
+var _h=document.head||document.getElementsByTagName("head")[0];
+_h.appendChild(_t);
+}).call(this);
